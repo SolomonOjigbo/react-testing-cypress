5e05822d6309514dc1367e33f9d27bc43674181f
SolomonOjigbo <solomonojigbo@gmail.com>
2022-08-18 16:30:35 +0100

    Testing the account balance checking feature

--- cypress/integration/payment_spec.js
+++ cypress/integration/payment_spec.js
@@ -6,7 +6,13 @@ describe("payment", () => {
     cy.findByLabelText(/password/i).type("s3cret");
     cy.findByRole("checkbox", { name: /remember me/i }).check();
     cy.findByRole("button", { name: /sign in/i }).click();
+
     // Check account balance
+    let currentBalance;
+    cy.get("[data-test=sidenav-user-balance]")
+      .then(($balance) => (currentBalance = $balance.text()))
+      .then((balance) => console.log(balance));
+
     // Click on pay button
     // Search for user
     // Add amount and note and click pay
