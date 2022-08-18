07cf4ddd888aa96f88521075660dabf1eb4578f4
SolomonOjigbo <solomonojigbo@gmail.com>
2022-08-18 15:30:56 +0100

    Ran integration test on the login flow using Cypress

---
+++ cypress/integration/payment_spec.js
@@ -0,0 +1,19 @@
+describe("payment", () => {
+  it("user can make payment", () => {
+    // Login
+    cy.visit("/");
+    cy.findByRole("textbox", { name: /username/i }).type("johndoe");
+    cy.findByLabelText(/password/i).type("s3cret");
+    cy.findByRole("checkbox", { name: /remember me/i }).check();
+    cy.findByRole("button", { name: /sign in/i }).click();
+    // Check account balance
+    // Click on pay button
+    // Search for user
+    // Add amount and note and click pay
+    // Return to transactions
+    // Go to personal payments
+    // Click on payment
+    // Verify if payment was made
+    // Verify if payment amount was deducted
+  });
+});
