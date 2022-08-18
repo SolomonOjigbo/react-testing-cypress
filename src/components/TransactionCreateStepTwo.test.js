11bd8136048e6a26592daefcde31344411496034
SolomonOjigbo <solomonojigbo@gmail.com>
2022-08-18 08:36:44 +0100

    Testing if the Pay button is enabled if an amount and note is entered

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
 test("On initial render, the pay button is disabled", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
 
   expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
+});
+
+test("If an amount and note is entered, the pay button becomes enabled", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+
+  userEvent.type(screen.getByPlaceholderText(/Amount/i), "50");
+  userEvent.type(screen.getByPlaceholderText(/Add a note/i), "lunch bill");
+
+  // screen.getByRole("");
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
 });
